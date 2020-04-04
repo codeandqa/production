e99aec8339bcd1dad2f0ce2ced6aa75ff1d29d34
codeandqa <aditya@anwisys.com>
2020-04-04 01:23:54 -0400

    Updates

---
+++ precache-manifest.9e76b74b25f8fccdcc3385cb6eddb7d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a936e5839e2c369d2bf",
+    "url": "/production/static/js/runtime~main.8a936e58.js"
+  },
+  {
+    "revision": "3b20563280bca24a8ee3",
+    "url": "/production/static/js/main.3b205632.chunk.js"
+  },
+  {
+    "revision": "ec00c7a502987c340d20",
+    "url": "/production/static/js/1.ec00c7a5.chunk.js"
+  },
+  {
+    "revision": "3b20563280bca24a8ee3",
+    "url": "/production/static/css/main.63f591f6.chunk.css"
+  },
+  {
+    "revision": "ec00c7a502987c340d20",
+    "url": "/production/static/css/1.1d7d1856.chunk.css"
+  },
+  {
+    "revision": "f5735cca1b5c606093c3be39ffdec7ac",
+    "url": "/production/index.html"
+  }
+];
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import beerReducer from "./beerReducer";
-
-// combine reducers into state
-export default combineReducers({
-  beer: beerReducer
-});
